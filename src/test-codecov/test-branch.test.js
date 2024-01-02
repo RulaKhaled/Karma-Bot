cb4690c31d7dd4e85a9db913be5271773084ce95
Rula Abu Hasna <rula@codecov.io>
2024-01-02 17:10:39 +0100

    Update with more flags

--- src/test-codecov/test-branch.test.js
+++ src/test-codecov/test-branch.test.js
@@ -4,17 +4,17 @@ test("returns the given array reversed", function () {
   expect(reverse([1, 2, 3])).toEqual([3, 2, 1]);
 });
 
-test("returns + 3 of whatever you give it", function () {
-  expect(returnPlus3(4)).toBe(7);
-});
+// test("returns + 3 of whatever you give it", function () {
+//   expect(returnPlus3(4)).toBe(7);
+// });
 
-test("random test", function () {
-  expect(randomTest()).toBe("hello");
-});
+// test("random test", function () {
+//   expect(randomTest()).toBe("hello");
+// });
 
-test("returns 9 when given 6", () => {
-  expect(returnPlus3(6)).toBe(9);
-});
+// test("returns 9 when given 6", () => {
+//   expect(returnPlus3(6)).toBe(9);
+// });
 
 test("returns 'hello!' if given 5", function () {
   expect(hello(5)).toBe("hello!");
@@ -26,4 +26,4 @@ test("returns 'nope' if given anything other than 5", function () {
 
 test("returns 'nope'", function () {
   expect(hello(9)).toBe("nope");
-})
+});
