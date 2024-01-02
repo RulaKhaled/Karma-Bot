fcb390a1606f8171d00e4b38e626ad5011f6a7b9
Rula Abu Hasna <rula@codecov.io>
2024-01-02 18:31:14 +0100

    update with tests

--- src/test-codecov/test-branch.test.js
+++ src/test-codecov/test-branch.test.js
@@ -4,13 +4,13 @@ test("returns the given array reversed", function () {
   expect(reverse([1, 2, 3])).toEqual([3, 2, 1]);
 });
 
-// test("returns + 3 of whatever you give it", function () {
-//   expect(returnPlus3(4)).toBe(7);
-// });
+test("returns + 3 of whatever you give it", function () {
+  expect(returnPlus3(4)).toBe(7);
+});
 
-// test("random test", function () {
-//   expect(randomTest()).toBe("hello");
-// });
+test("random test", function () {
+  expect(randomTest()).toBe("hello");
+});
 
 // test("returns 9 when given 6", () => {
 //   expect(returnPlus3(6)).toBe(9);
