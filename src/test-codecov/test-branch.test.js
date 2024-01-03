9edd4dcdf9e391a323703fdefd7bea18a9a16532
Rula Abu Hasna <rula@codecov.io>
2024-01-03 11:13:17 +0100

    Update test

--- src/test-codecov/test-branch.test.js
+++ src/test-codecov/test-branch.test.js
@@ -20,10 +20,10 @@ test("returns 'hello!' if given 5", function () {
   expect(hello(5)).toBe("hello!");
 });
 
-test("returns 'nope' if given anything other than 5", function () {
-  expect(hello(4)).toBe("nope");
-});
+// test("returns 'nope' if given anything other than 5", function () {
+//   expect(hello(4)).toBe("nope");
+// });
 
 test("returns 'nope'", function () {
   expect(hello(9)).toBe("nope");
-})
+});
